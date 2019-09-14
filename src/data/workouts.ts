fab2a4ea9641d8042c792a14a562e4bfd180ed1c
Anders <819213+andersr@users.noreply.github.com>
2019-09-14 16:33:10 -0400

    organize workouts into typed / other

--- src/data/workouts.ts
+++ src/data/workouts.ts
@@ -15,100 +15,113 @@ interface Workout extends Card {
   type: EWorkoutType;
 }
 
-export const WORKOUTS: Workout[] = [
-  {
-    title: "Bicycle crunches",
-    type: EWorkoutType.CRUNCH
-  },
-  {
-    title: "Burpees",
-    description: "With pushup?",
-    type: EWorkoutType.PUSHUP
-  },
+
+export const TYPED_WORKOUTS = {
+  [EWorkoutType.BURPEE]: [
+    {
+      title: "Half burpees",
+      type: EWorkoutType.BURPEE
+    },
+  ],
+  [EWorkoutType.CRUNCH]: [
+    {
+      title: "Bicycle crunches",
+      type: EWorkoutType.CRUNCH
+    },
+    {
+      title: "Regular crunches",
+      type: EWorkoutType.CRUNCH
+    },
+  ],
+  [EWorkoutType.LUNGE]: [
+    {
+      title: "Forward Lunges",
+      type: EWorkoutType.LUNGE
+    },
+    {
+      title: "Backward Lunges",
+      type: EWorkoutType.LUNGE
+    },
+  ],
+  [EWorkoutType.MOUNTAIN_CLIMBER]: [
+    {
+      title: "Mountain Climbers",
+      type: EWorkoutType.MOUNTAIN_CLIMBER
+    },
+  ],
+  [EWorkoutType.PLANK]: [
+    {
+      title: "Plank",
+      type: EWorkoutType.PLANK
+    },
+    {
+      title: "High Plank",
+      type: EWorkoutType.PLANK
+    },
+    {
+      title: "Plank with knee tap",
+      type: EWorkoutType.PLANK
+    },
+    {
+      title: "Side to side plank",
+      type: EWorkoutType.PLANK
+    },
+  ],
+  [EWorkoutType.PUSHUP]: [
+    {
+      title: "Plank Pushups",
+      description: "High to low",
+      type: EWorkoutType.PUSHUP
+    },
+    {
+      title: "Pushups",
+      type: EWorkoutType.PUSHUP
+    },
+  ],
+  [EWorkoutType.SQUAT]: [
+    {
+      title: "Regular Squats",
+      type: EWorkoutType.SQUAT
+    },
+    {
+      title: "Jumping Squats",
+      type: EWorkoutType.SQUAT
+    },
+  ],
+}
+
+export const OTHER_WORKOUTS = [
   {
     title: "Butt kickers",
-    type: EWorkoutType.OTHER
-  },
-  {
-    title: "Crunches",
-    type: EWorkoutType.CRUNCH
-  },
-  {
-    title: "Half burpees",
-    type: EWorkoutType.BURPEE
+    type: EWorkoutType.OTHER,
   },
   {
     title: "High knees",
-    type: EWorkoutType.OTHER
+    type: EWorkoutType.OTHER,
   },
   {
     title: "Jumping Jacks",
-    type: EWorkoutType.OTHER
-  },
-  {
-    title: "Jumping Squats",
-    type: EWorkoutType.SQUAT
+    type: EWorkoutType.OTHER,
   },
   {
     title: "Leg lifts",
-    type: EWorkoutType.OTHER
-  },
-  {
-    title: "Lunges (forward)",
-    type: EWorkoutType.LUNGE
-  },
-  {
-    title: "Mountain Climbers",
-    type: EWorkoutType.MOUNTAIN_CLIMBER
-  },
-  {
-    title: "Plank",
-    type: EWorkoutType.PLANK
-  },
-  {
-    title: "High Plank",
-    type: EWorkoutType.PLANK
-  },
-  {
-    title: "Plank with knee tap",
-    type: EWorkoutType.PLANK
-  },
-  {
-    title: "Plank - side to side",
-    type: EWorkoutType.PLANK
-  },
-  {
-    title: "Plank Pushups",
-    description: "High to low",
-    type: EWorkoutType.PLANK
-  },
-  {
-    title: "Pushups",
-    type: EWorkoutType.PUSHUP
+    type: EWorkoutType.OTHER,
   },
   {
     title: "Rollers",
     description: "Equipment needed",
-    type: EWorkoutType.OTHER
+    type: EWorkoutType.OTHER,
   },
   {
     title: "Situps",
-    type: EWorkoutType.OTHER
-  },
-  {
-    title: "Squats",
-    type: EWorkoutType.SQUAT
+    type: EWorkoutType.OTHER,
   },
   {
     title: "Superman",
-    type: EWorkoutType.OTHER
-  },
-  {
-    title: "Mountain Climbers",
-    type: EWorkoutType.MOUNTAIN_CLIMBER
+    type: EWorkoutType.OTHER,
   },
   {
     title: "Russian Twist",
-    type: EWorkoutType.OTHER
+    type: EWorkoutType.OTHER,
   }
-];
+]
