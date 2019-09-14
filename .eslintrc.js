87e6c67b916dd6635bd034fb0f2d8f2ff9982fb5
Anders <819213+andersr@users.noreply.github.com>
2019-09-14 16:33:10 -0400

    eslintrc formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,47 +1,43 @@
 module.exports = {
   env: {
     "react-native/react-native": true,
-    es6: true,
+    es6: true
   },
   extends: [
-    'plugin:react/recommended',  
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', 
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended"
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
-  plugins: [
-    'react',
-    'react-native',
-    '@typescript-eslint',
-  ],
+  plugins: ["react", "react-native", "@typescript-eslint"],
   rules: {
-      // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
   },
-  "overrides": [
+  overrides: [
     {
-        "files": ["**/*.tsx"],
-        "rules": {
-            "react/prop-types": "off"
-        }
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off"
+      }
     }
-],
-  settings:  {
-    react:  {
-      version:  'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
-    },
-  },
+  ],
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
+  }
 };
