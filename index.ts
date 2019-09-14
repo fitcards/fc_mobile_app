21809a1db18ee90b7f970267bba4f7c2780cc566
Anders <819213+andersr@users.noreply.github.com>
2019-09-14 16:33:10 -0400

    get types array

---
+++ index.ts
@@ -0,0 +1,8 @@
+// created from 'create-ts-index'
+
+export * from './.expo';
+export * from './.expo-shared';
+export * from './.git';
+export * from './assets';
+export * from './src';
+export * from './App';
